aaa0f7279866982e076e0d813ea0a7309536c8d1
FabioSansone <fabiosansone33@gmail.com>
2025-11-17 13:50:23 +0100

    Solved a small bug

--- client/evproducer.sh
+++ client/evproducer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 SERVER_IP=$1
-evproducer_pid=$(nohup /opt/mpmt-readout/evproducer --host $SERVER_IP --disable-rc > /dev/null 2>&1 & echo $!)
+evproducer_pid=$(nohup /opt/mpmt-readout/evproducer --host $SERVER_IP > /dev/null 2>&1 & echo $!)
 echo "Started evproducer with PID $evproducer_pid and server IP $SERVER_IP"
 exit
